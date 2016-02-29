a83985bd7b21da8578d1fc64d126b3567148b183
sriramrajaraman <sriramrajaraman@users.noreply.github.com>
2016-02-29 13:59:46 -0500

    Update init.js

--- client/init.js
+++ client/init.js
@@ -6,23 +6,20 @@ var io = require('socket.io')(http);
 var scanner = io.of('/'); 
 
 scanner.on('connection', function(socket) {
+  console.log('Scanner Connected');
+  socket.on('deviceData', function(msg) {
+    //recived message from scanner
+    socket.compress(false).broadcast.emit('deviceData',msg);
+    console.log(msg);
+  });
 
-    console.log('Scanner Connected');
-    
-    socket.on('deviceData', function(msg) {
-        //recived message from scanner
-        //do some processing here
-				socket.compress(false).broadcast.emit('deviceData',msg);
-				console.log(msg);
-    });
-
-    socket.on('disconnect', function() {
-        console.log('Scanner Disconnected');
-    });
+  socket.on('disconnect', function() {
+    console.log('Scanner Disconnected');
+  });
 });
 
 http.listen(3000, function() {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
 
 var path = require('path');
